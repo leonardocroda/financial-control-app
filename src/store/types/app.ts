74a71e36d72b9a64ded76a611056e7b459bc4c87
Leonardo Croda <lccroda@gmail.com>
2021-10-04 22:34:42 -0300

    update google-services.json

--- src/store/types/app.ts
+++
@@ -1,5 +0,0 @@
-import { User } from './auth';
-
-export type AppState = {
-  user: User;
-};
--- src/store/types/category.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './auth';
-
-export type Category = {
-  id: number;
-  name: string;
-  color: string;
-  isActive: boolean;
-  user: User;
-};
